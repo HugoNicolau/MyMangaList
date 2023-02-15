bfbb4a6c2d7f4739330c089ba28548f4e35f357a
HugoNicolau <nicolau.hugogiles@gmail.com>
2023-02-15 13:35:44 -0300

    fix: correcting dotenv and database connection

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,11 +1,23 @@
-import pkg from "pg";
 import dotenv from "dotenv";
-
+import pkg from "pg";
 
 dotenv.config();
 const { Pool } = pkg;
 
+// export const connectionDB = new Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'root',
+//     database: 'my_manga_list'
+//   });
+
 export const connectionDB = new Pool( {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host: process.env.DATABASE_URL,
+    port: Number(process.env.DATABASE_PORT),
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+
+    // ssl: true
 });
