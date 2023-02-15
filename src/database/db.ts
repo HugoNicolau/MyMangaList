6f99c4e80e73dab021c5142b3946299ce04fd577
HugoNicolau <nicolau.hugogiles@gmail.com>
2023-02-15 20:38:02 -0300

    refact: Remove comments

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,14 +4,6 @@ import pkg from "pg";
 dotenv.config();
 const { Pool } = pkg;
 
-// export const connectionDB = new Pool({
-//     host: 'localhost',
-//     port: 5432,
-//     user: 'postgres',
-//     password: 'root',
-//     database: 'my_manga_list'
-//   });
-
 export const connectionDB = new Pool( {
     host: process.env.DATABASE_URL,
     port: Number(process.env.DATABASE_PORT),
